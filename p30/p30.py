d303c6af7ac99c2b36336ab4ffaf2c677742db65
zachwe <zbwener@gmail.com>
2014-12-03 00:05:42 -0500

    solved problem 30. Should check if there's a better way tho this is ok.

---
+++ p30/p30.py
@@ -0,0 +1,14 @@
+
+# quick and dirty. let's do this. 
+total = 0
+for i in range(10, 420000):
+    n = i
+    tmptotal = 0
+    while n:
+        tmptotal += (n % 10) ** 5
+        n = n / 10
+    if tmptotal == i:
+        total += i
+print total
+
+
